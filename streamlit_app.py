7517fce0c0956b640925878488755d0ca406c69c
asmin <42002571+asminalev@users.noreply.github.com>
2023-11-23 01:18:55 +0300

    Update streamlit_app.py
    error is resolved

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,7 +34,7 @@ streamlit.dataframe(fruityvice_normalized)
 import snowflake.connector
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
-my_cur.execute("SELECT * fruit_load_list")
+my_cur.execute("SELECT * FROM fruit_load_list")
 my_data_row = my_cur.fetchone()
 streamlit.text("Thr fruit load list contains:")
 streamlit.text(my_data_row)
